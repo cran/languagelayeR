646b5f4939d1dcf01fbd83983cf1fc5d236fbdac
Colin FAY <contact@colinfay.me>
2019-02-12 09:03:19 +0000

    version 1.2.4

---
+++ NEWS.md
@@ -0,0 +1,14 @@
+---
+title: "NEWS"
+output: html_document
+---
+
+## languagelayeR 1.2.0
+
+Old deprecated functions are not available anymore.
+
+## languagelayeR 1.1.0
+
+`setApiKey` is now deprecated
+`getSupportedLanguage` is now `get_supported_lang`
+`getLanguage` is now `get_lang`
--- inst/doc/Introduction.R
+++ inst/doc/Introduction.R
@@ -2,7 +2,7 @@
 #  install.packages("languagelayeR")
 
 ## ----eval = FALSE--------------------------------------------------------
-#  devtools::install_github("ColinFay/languagelayeR")
+#  remotes::install_github("ColinFay/languagelayeR")
 
 ## ----eval = FALSE--------------------------------------------------------
 #  get_lang(query = "I really really love R and that's a good thing, right?", api_key = "your_api_key")
